f8108d0ccf7bc51d787e3f47613388ee28957870
Hilal2013 <hilalozkul@gmail.com>
2023-01-15 22:37:42 -0500

    updated

--- src/StaticConstants.java
+++ src/StaticConstants.java
@@ -4,4 +4,6 @@ import java.util.List;
 public class StaticConstants {
 
     public static final List<Customer> CUSTOMER_LIST=new ArrayList<>();
+//final because this is database it shouldn't be changed at all
+
 }
