5842346e8468e34b4187bce6f33db35d7741ce3b
Hilal2013 <hilalozkul@gmail.com>
2023-01-16 13:10:42 -0500

    added Product class

---
+++ src/Product.java
@@ -0,0 +1,42 @@
+import java.util.UUID;
+
+public class Product {
+    private UUID id;
+    private String name;
+    private Double price;
+    private Integer stock, remainingStock;
+    public UUID categoryID;
+
+    public Product(UUID id, String name, Double price, Integer stock, Integer remainingStock, UUID categoryID) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.remainingStock = remainingStock;
+        this.categoryID = categoryID;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public Integer getRemainingStock() {
+        return remainingStock;
+    }
+
+    public UUID getCategoryID() {
+        return categoryID;
+    }
+}
